c32e9ba205c9df4e2fd975d7b35f1ce1654054a2
victim <149972831+alluding@users.noreply.github.com>
2024-03-04 05:39:11 -0500

    Improved :0

--- hanime/models/video.py
+++ hanime/models/video.py
@@ -1,8 +1,7 @@
-from typing import List, Optional
+from typing import List, Optional, Dict, Any, Union
+
 from pydantic import BaseModel
-
 from bs4 import BeautifulSoup
-
 
 class HanimeVideoData(BaseModel):
     title: str
@@ -10,12 +9,11 @@ class HanimeVideoData(BaseModel):
     tags: List[str]
     description: Optional[str]
 
-
 class HanimeVideo:
-    def __init__(self, data: dict):
+    def __init__(self, data: Dict[str, Any]):
         self.data = data
 
-    def __getattr__(self, name: str):
+    def __getattr__(self, name: str) -> Any:
         return self.data.get(name, None)
 
     @property
@@ -23,3 +21,6 @@ class HanimeVideo:
         html_description = self.data.get('description', '')
         soup = BeautifulSoup(html_description, 'html.parser')
         return soup.get_text() if html_description else None
+
+    def to_dict(self) -> Dict[str, Any]:
+        return self.data
