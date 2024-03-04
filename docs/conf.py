af90080c18261b4b60971d67a8f202931730624b
lucifer <parkertristan830@gmail.com>
2024-03-04 12:37:49 +0000

    update

--- docs/conf.py
+++ docs/conf.py
@@ -7,6 +7,33 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.linkcode",
+    "myst_parser",
+]
+
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+
+myst_heading_anchors = 3
+# thanks pomice (cloudwithax) :]
+
+
 html_theme = 'furo'
 html_static_path = ['_static']
 
