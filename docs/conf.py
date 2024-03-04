628ac3121bddc9af59b50f5881ede75f646f0548
lucifer <parkertristan830@gmail.com>
2024-03-04 12:40:37 +0000

    nooo

--- docs/conf.py
+++ docs/conf.py
@@ -1,21 +1,24 @@
-project = 'hanime'
-author = 'alluding'
-version = '2.0.0'
-copyright = f'Copyright © {author}'
+import importlib
+import inspect
+import os
+from typing import Any, Dict, List, Optional
 
-extensions = []
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+project: str = 'hanime'
+author: str = 'alluding'
+version: str = '2.0.0'
+copyright: str = f'Copyright © {author}'
 
-
-extensions = [
+extensions: List[str] = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.linkcode",
     "myst_parser",
 ]
 
-myst_enable_extensions = [
+templates_path: List[str] = ['_templates']
+exclude_patterns: List[str] = ['_build', 'Thumbs.db', '.DS_Store']
+
+myst_enable_extensions: List[str] = [
     "amsmath",
     "colon_fence",
     "deflist",
@@ -30,12 +33,43 @@ myst_enable_extensions = [
     "tasklist",
 ]
 
-myst_heading_anchors = 3
-# thanks pomice (cloudwithax) :]
+myst_heading_anchors: int = 3
+
+html_theme: str = 'furo'
+html_static_path: List[str] = ['_static']
+
+source_suffix: str = '.md'
+master_doc: str = 'index'
 
 
-html_theme = 'furo'
-html_static_path = ['_static']
+def linkcode_resolve(domain: str, info: Dict[str, Any]) -> Optional[str]:
+    try:
+        if domain != "py" or not info["module"]:
+            return None
 
-source_suffix = '.md'
-master_doc = 'index'
+        mod = importlib.import_module(info["module"])
+
+        if "." in info["fullname"]:
+            objname, attrname = info["fullname"].split(".")
+            obj = getattr(mod, objname)
+
+            try:
+                obj = getattr(obj, attrname)
+            except AttributeError:
+                return None
+        else:
+            obj = getattr(mod, info["fullname"])
+
+        try:
+            file = inspect.getsourcefile(obj)
+            lines = inspect.getsourcelines(obj)
+        except TypeError:
+            # e.g., object is a typing.Union
+            return None
+
+        file = os.path.relpath(file, os.path.abspath(".."))
+        start, end = lines[1], lines[1] + len(lines[0]) - 1
+
+        return f"https://github.com/alluding/hanime/blob/main/{file}#L{start}-L{end}"
+    except Exception:
+        return None
