db43a0a9815bf0a830443156c0f8ac330f2afdfc
lucifer <parkertristan830@gmail.com>
2024-03-04 12:28:02 +0000

    ok

---
+++ docs/conf.py
@@ -0,0 +1,11 @@
+project = 'hanime'
+author = 'alluding'
+version = '2.0.0'
+copyright = f'Copyright © {author}'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'furo'
+html_static_path = ['_static']
